3388a1de2c596dca52afca4629b4e1e9d71ffc5c
Igor <igoralann113@gmail.com>
2024-10-29 19:55:59 +0900

    first commit

---
+++ main.py
@@ -0,0 +1,121 @@
+import requests
+from bs4 import BeautifulSoup
+import xlwt
+import os
+from datetime import datetime
+import imghdr
+from fake_useragent import UserAgent
+import json
+import urllib.parse
+
+base_url = "https://www.bjs.com"
+section_id = 1
+page = 1
+length = 1
+
+def get_lists():
+    global base_url, length, page, section_id
+    ua = UserAgent()
+    headers = {
+        'User-Agent': ua.random
+    }
+    products = []
+    
+    while length != 0:
+        params = {
+            "c": "ciojs-client-2.53.1",
+            "key": "key_2i36vP8QTs3Ati4x",
+            "i": "10b89e05-00d7-4c19-8cd4-8f49913a8cd8",
+            "s": 1,
+            "page": page,
+            "num_results_per_page": 200,
+            "pre_filter_expression": json.dumps({"or":[{"name":"avail_stores","value":"online"},{"name":"avail_stores","value":"0096"},{"and":[{"name":"avail_stores","value":"0096"},{"name":"avail_sdd","value":"0096"}]},{"name":"out_of_stock","value":"Y"}]})
+        }
+        try:
+            response = requests.get("https://ac.cnstrc.com/browse/group_id/all", params=params, headers=headers)
+            
+            data = response.json()
+            for result in data["response"]["results"]:
+                response = requests.get(base_url + result["data"]["url"], params=params, headers=headers)
+                soup = BeautifulSoup(response.content, 'html.parser')
+                script_tag = soup.find('script', type='application/ld+json')
+                if script_tag:
+                    script_content = script_tag.string
+                    decoded_content = urllib.parse.unquote(script_content)
+                    json_data = json.loads(decoded_content)
+                    record = [
+                        str(section_id),
+                        base_url,
+                        base_url + result["data"]["url"],
+                        "BJS",
+                        json_data["brand"]["name"],
+                        json_data["description"],
+                        json_data["name"],
+                        "",
+                        "",
+                        json_data["offers"][0].get("price", ""),
+                        "",
+                        json_data["image"],
+                        "",
+                        "",
+                        json_data["aggregateRating"].get("ratingValue", ""),
+                        json_data["aggregateRating"].get("reviewCount", ""),
+                        "Westborough, MA",
+                        "+1(800)257-2582",
+                        "42.2695",
+                        "-71.6162",
+                        "",
+                    ]
+                
+                    products.append(record)
+                    section_id = section_id + 1
+                    print(record)
+            length = len(data["response"]["results"])
+            
+            # response = requests.get("https://www.bjs.com/product/berkley-jensen-ultra-dishwashing-liquid-dish-soap-100-oz/3000000000004955305", params=params, headers=headers)
+            # with open('index.html', 'w', encoding='utf-8') as file:
+            #     file.write(response.text)
+            
+            page = page + 1
+        except Exception as e:
+            print(e)
+    return products
+        
+    
+
+# Step 3: Main function
+if __name__ == '__main__':
+    titleData = ["id","Store page link", "Product item page link", "Store_name", "Category", "Product_description", "Product Name", "Weight/Quantity", "Units/Counts", "Price", "image_file_names", "Image_Link", "Store Rating", "Store Review number", "Product Rating", "Product Review number", "Address", "Phone number", "Latitude", "Longitude", "Description Detail"]
+    widths = [10,50,50,60,45,70,35,25,25,20,130,130,30,30,30,30,60,50,60,60,80]
+    style = xlwt.easyxf('font: bold 1; align: horiz center')
+    products = []
+
+    if(not os.path.isdir("products")):
+        os.mkdir("products")
+
+    now = datetime.now()
+    current_time = now.strftime("%m-%d-%Y-%H-%M-%S")
+    prefix = now.strftime("%Y%m%d%H%M%S%f_")
+    os.mkdir("products/"+current_time)
+    os.mkdir("products/"+current_time+"/images")
+    
+    records = get_lists()
+    
+    workbook = xlwt.Workbook()
+    sheet = workbook.add_sheet('Sheet1')
+
+    for col_index, value in enumerate(titleData):
+        first_col = sheet.col(col_index)
+        first_col.width = 256 * widths[col_index]  # 20 characters wide
+        sheet.write(0, col_index, value, style)
+        
+    for row_index, row in enumerate(records):
+        for col_index, value in enumerate(row):
+            sheet.write(row_index+1, col_index, value)
+
+    # Save the workbook
+    workbook.save("products/"+current_time+"/products.xls")
+    
+    
+    
+    
